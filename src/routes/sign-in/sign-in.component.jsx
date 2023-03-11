1edacd8c577508bb53104ea2fe2bd9aec8b1544a
Alex Valdez <xvaldez0411@gmail.com>
2023-03-11 14:43:37 -0600

    added firebase authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,18 @@
+import{ signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+
 
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
   return (
     <div>
-        Sign In Page
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>
+            Sign in with Google Popup
+        </button>
     </div>
   )
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCvWlHvLsKLZd8vR2N7gOFrG1f7fugs4T8",
+    authDomain: "clothing-db-963e9.firebaseapp.com",
+    projectId: "clothing-db-963e9",
+    storageBucket: "clothing-db-963e9.appspot.com",
+    messagingSenderId: "836707728659",
+    appId: "1:836707728659:web:7a2e4d817d69b9c350aee6"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+  
+  provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
