5f43ed60aa56a048294b12a36b93a43938a1a3ab
Alex Valdez <xvaldez0411@gmail.com>
2023-03-13 16:20:50 -0500

    made changes to firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -34,4 +34,21 @@ const firebaseConfig = {
     const userSnapshot = await getDoc(userDocRef);
     console.log(userSnapshot);
     console.log(userSnapshot.exists());
-  }
+
+    if(!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message)
+      }
+    }
+
+    return userDocRef;
+  };
