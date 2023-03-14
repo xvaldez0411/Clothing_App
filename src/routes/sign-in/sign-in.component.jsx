5375eaf5d537d3350e28ab7cef58939895d4fa79
Alex Valdez <xvaldez0411@gmail.com>
2023-03-13 20:54:59 -0500

    implemented google pop up sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,24 @@
-import{ signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import{ 
+  signInWithGooglePopup, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils'
 
 
 const SignIn = () => {
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
-        createUserDocumentFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user)
     }
+
+
   return (
     <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>
             Sign in with Google Popup
         </button>
+
     </div>
   )
 }
