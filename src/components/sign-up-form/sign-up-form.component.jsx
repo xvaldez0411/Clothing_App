bdcf16d9da243319379436de6c1b0aaf6767b5d5
Alex Valdez <xvaldez0411@gmail.com>
2023-03-14 23:05:31 -0500

    created sign up component, form component, button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import './sign-up-form.styles.scss'
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
@@ -55,8 +58,9 @@ const SignUpForm = () => {
     };
 
     return(
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit = {handleSubmit}>
 
                 <FormInput
@@ -69,7 +73,7 @@ const SignUpForm = () => {
                 />
 
                 <FormInput
-                    label="email"
+                    label="Email"
                     type="email" 
                     required 
                     onChange={handleChange} 
@@ -78,7 +82,7 @@ const SignUpForm = () => {
                 />
                 
                 <FormInput
-                    label="password"
+                    label="Password"
                     type="password" 
                     required 
                     onChange={handleChange} 
@@ -95,7 +99,7 @@ const SignUpForm = () => {
                     value={confirmPassword} 
                 />
 
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
