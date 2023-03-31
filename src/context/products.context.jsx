ce40667527d48ef758a7c57383359f78d2a8cba2
Alex Valdez <xvaldez0411@gmail.com>
2023-03-31 14:12:47 -0500

    added in library in firebase utils, made changes in products.context and shop-data.js

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,14 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from '../shop-data.json'
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
     products: [],
-
 });
 
-export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+export const ProductsProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();
+    }, [])
     const value = {products};
     return (
         <ProductsContext.Provider value={value} >{children}</ProductsContext.Provider>
