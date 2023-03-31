bc5d286c0d15cc0735421a5aba0901498f478109
Alex Valdez <xvaldez0411@gmail.com>
2023-03-31 15:23:57 -0500

    made changes to index.js and shop.component, added in library from firebase

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,30 @@
-import { useContext } from 'react'
+import { useContext, Fragment } from 'react'
 import './shop.styles.scss';
 
-import { ProductsContext } from '../../context/products.context'
+import { CategoriesContext } from '../../context/categories.context'
 import ProductCard from '../../components/product-card/product-card.component';
 
 const Shop = () => {
 
-    const {products} = useContext(ProductsContext);
+    const { categoriesMap } = useContext(CategoriesContext);
 
     return (
-        <div className='products-container'>
+        <Fragment>
             {
-                products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
+                Object.keys(categoriesMap).map(title => (
+                    <Fragment key={title}>
+                        <h2>{title}</h2>
+                        <div className='products-container'>
+                            {
+                                categoriesMap[title].map((product) => (
+                                    <ProductCard key={product.id} product={product} />
+                                ))
+                            }
+                        </div>
+                    </Fragment>
                 ))
             }
-        </div>
+        </Fragment>
     )
 }
 
