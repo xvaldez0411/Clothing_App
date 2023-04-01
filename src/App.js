049f25ff66c44946ed8dc6234a0a023ccb533290
Alex Valdez <xvaldez0411@gmail.com>
2023-04-01 10:39:58 -0500

    added component file in components and routes, implemented categories with Link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         {/* added index makes the HOME component a direct child of the "/" path  */}
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop/>} />
+        <Route path='shop/*' element={<Shop/>} />
         <Route path='auth' element={<Authentication/>} />
         <Route path='checkout' element={<Checkout/>} />
       </Route>
